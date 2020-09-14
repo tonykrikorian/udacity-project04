f3161c38c9561d34c9d84a40722e126e0d6762de
tkrikoriam <tony.krikorian@outlook.com>
2020-09-14 10:23:16 -0300

    Add dockerfile

--- run_docker.sh
+++ run_docker.sh
@@ -4,9 +4,12 @@
 
 # Step 1:
 # Build image and add a descriptive tag
+docker build -t python:3.7.3-stretch3 .
 
 # Step 2: 
 # List docker images
+docker images
 
 # Step 3: 
 # Run flask app
+docker run -d -p8000:80 --name udacity-python-ms python:3.7.3-stretch3
