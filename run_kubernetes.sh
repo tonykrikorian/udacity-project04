2c1461a1dca2ce962b2e22d5e82a8ecf4ab1c219
tkrikoriam <tony.krikorian@outlook.com>
2020-09-16 08:48:56 -0300

    cxcxcx

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,22 @@
 
 # Step 1:
 # This is your Docker ID/path
-  dockerpath=$IMAGE_NAME_TAG
-
+  dockerpath="tkrikoriam/tk-api:python-v1.0.102"
+  deploymentName="kubernetes-udacity"
 # Step 2
 # Run the Docker Hub container with kubernetes
-kubectl 
+kubectl create deployment kubernetes-udacity --image=$dockerpath
+kubectl get deployments
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl expose deployment/$deploymentName --type="NodePort" --port 80
+kubectl get services
 
+
+#kubectl delete service/kubernetes-udacity
+#kubectl delete deploy/kubernetes-udacity
