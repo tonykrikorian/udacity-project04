b69b57410d6d2bdf601d1fd7c8bae5e4e81bde24
tkrikoriam <tony.krikorian@outlook.com>
2020-09-20 11:08:41 -0300

    lint perfect

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ LOG.setLevel(logging.INFO)
 def scale(payload):
     """Scales Payload"""
 
-    LOG.info(f"Scaling Payload: \n{payload}")
+    LOG.info("Scaling Payload: \n %s", payload)
     scaler = StandardScaler().fit(payload.astype(float))
     scaled_adhoc_predict = scaler.transform(payload.astype(float))
     return scaled_adhoc_predict
@@ -59,14 +59,14 @@ def predict():
 
     # Logging the input payload
     json_payload = request.json
-    LOG.info(f"JSON payload: \n{json_payload}")
+    LOG.info("JSON payload: \n%s", json_payload)
     inference_payload = pd.DataFrame(json_payload)
-    LOG.info(f"Inference payload DataFrame: \n{inference_payload}")
+    LOG.info("Inference payload DataFrame: \n%s", inference_payload)
     # scale the input
     scaled_payload = scale(inference_payload)
-    LOG.info(f"Scaled payload DataFrame: \n{scaled_payload}")
+    LOG.info("Scaled payload DataFrame: \n%s", scaled_payload)
     # get an output prediction from the pretrained model, clf
-    LOG.info(f"Server hostname is {os.getenv('HOSTNAME')}")
+    LOG.info("Server hostname is %s", os.getenv('HOSTNAME'))
     prediction = list(clf.predict(scaled_payload))
     # TO DO:  Log the output prediction value
     return jsonify({'prediction': prediction})
